15f239533da0b2fec7f93de87c81db8327d4570b
Ornit Cohen Gindi <ornit.cgindi@gmail.com>
2025-04-10 20:50:47 +0300

    better heuristic writing

--- Tiles.py
+++ Tiles.py
@@ -52,8 +52,8 @@ def display_output(alg, last_node):
 if __name__ == "__main__":
     # tiles_list = get_tiles_from_user()  # comment this and uncomment one of the next lines to test
     # tiles_list = [2,3,6,8,7,1,5,0,4]  # Example input  # no solution found
-    tiles_list = [2,0,6,8,7,1,5,3,4]  # Example input  # has solution
-    # tiles_list = [1,0,2,3,4,8,6,5,7]  # Example input  # has solution 7 steps
+    # tiles_list = [2,0,6,8,7,1,5,3,4]  # Example input  # has solution
+    tiles_list = [1,0,2,3,7,8,6,4,5]  # Example input  15 steps
 
     # print("Tiles list:", tiles_list)  #TODO DEBUG REMOVE
     try:
@@ -65,9 +65,9 @@ if __name__ == "__main__":
 
 
             # run the BFS algorithm
-            # bfs = BFS(state_space, start_state)  # TODO uncomment
-            # last_node = bfs.breadth_first_search()  # TODO uncomment
-            # display_output(bfs, last_node)  # TODO uncomment
+            bfs = BFS(state_space, start_state)  # TODO uncomment
+            last_node = bfs.breadth_first_search()  # TODO uncomment
+            display_output(bfs, last_node)  # TODO uncomment
 
             print() #line down
 
